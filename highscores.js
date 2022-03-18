73244003c062f7459519db1c673872f641db9292
nivinjohn37 <getnivin@gmail.com>
2022-03-18 16:17:01 +0530

    end

--- highscores.js
+++ highscores.js
@@ -3,6 +3,7 @@ const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
 highScores.map(score => {
   const li = document.createElement("li");
+  li.className ="high-score";
   li.innerText = `${score.name} - ${score.score}`;
   highScoresList.appendChild(li);
 });
